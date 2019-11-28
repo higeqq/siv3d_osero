64f443cc26ece9a82a35a18e0f0727ad1dc64392
kato sho <nascoqq@gmail.com>
2019-11-28 18:01:09 +0900

    インデント修正兼座標指定

--- Main.cpp
+++ Main.cpp
@@ -15,45 +15,37 @@ void Waku() {
     }
 }
 
-
-void user_change() {
-    Circle(300,150,40).draw();
-    
-}
-
+void user_change() { Circle(300, 150, 40).draw(); }
 
 void Main() {
-    int color [8][8] ;
-//    int i2 = 0;
-//    Rect x1(223,103,45,45);
-//    Rect x2(273,103,45,45);
+    int color[8][8];
+    //    int i2 = 0;
+    //    Rect x1(223,103,45,45);
+    //    Rect x2(273,103,45,45);
     Array<Rect> x;
     Array<Circle> d;
-    for (int i = 0; i < 8; i ++) {
-        for (int j = 0; j < 8; j++) {
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
             color[i][j] = 0;
-            x << Rect(223+50*j, 103+50*i,45,45);
-            d << Circle(245+50*j, 125+50*i,15);
+            x << Rect(223 + 50 * j, 103 + 50 * i, 45, 45);
+            d << Circle(245 + 50 * j, 125 + 50 * i, 15);
         }
     }
-    
-//    Circle d(245,125,15);
-//    Circle d2(295,125,15);
+
+    //    Circle d(245,125,15);
+    //    Circle d2(295,125,15);
     while(System::Update()) {
         Waku();
-        
-        for (int i = 0; i < 8; i ++) {
-               for (int j = 0; j < 8; j++) {
-                   if (x[i + j * 8].leftClicked()) {
-                       color[i][j] = 1;
-                   }
-                   if (color[i][j] == 1) {
-                       d[i + j * 8].draw();
-                   }
-               }
-           }
-//        x1.drawFrame();
-//        x2.drawFrame();
-        
+
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(x[i + j * 8].leftClicked()) {
+                    color[i][j] = 1;
+                }
+                if(color[i][j] == 1) {
+                    d[i + j * 8].draw();
+                }
+            }
+        }
     }
 }
