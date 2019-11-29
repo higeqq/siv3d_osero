7f7d216392d14e311806346839ae3a3eaf43d621
kato sho <nascoqq@gmail.com>
2019-11-29 14:45:34 +0900

    交互に置く処理完成

--- Main.cpp
+++ Main.cpp
@@ -14,12 +14,12 @@ void Waku() {
         Line(tate, 105, tate, 500).draw(4, Palette::White);
     }
 }
-
-void user_change() { Circle(300, 150, 40).draw(); }
-
+void syoki() { Circle(380, 280, 15).draw(); }
 void Main() {
+    int count = 1;
     int color[8][8];
-    //    int i2 = 0;
+    int white = 1;
+    int blue = 2;
     //    Rect x1(223,103,45,45);
     //    Rect x2(273,103,45,45);
     Array<Rect> x;
@@ -29,23 +29,42 @@ void Main() {
             color[i][j] = 0;
             x << Rect(223 + 50 * j, 103 + 50 * i, 45, 45);
             d << Circle(245 + 50 * j, 125 + 50 * i, 15);
+            //            blue << Circle(245 + 50 * j, 125 + 50 * i, 15);
         }
     }
+    color[3][3] = 1;
+    color[3][4] = 2;
+    color[4][3] = 2;
+    color[4][4] = 1;
 
-    //    Circle d(245,125,15);
-    //    Circle d2(295,125,15);
+    // jが縦　iが横
+
     while(System::Update()) {
+        // カウントして、割ったあまりが1なら白と、2なら青と (coutn % 2 ==
+        // 0なら白と)(count %2 == 1なら青とひ表示
         Waku();
-
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
                 if(x[i + j * 8].leftClicked()) {
-                    color[i][j] = 1;
+                    count++;
+                    if(count % 2 == 0) {
+                        color[i][j] = white;
+                        //                        if(color[i][j - 2] == white &&
+                        //                           color[i][j - 1] == blue) {
+                        //                            color[i][j - 1] = white;
+
+                    } else {
+                        color[i][j] = blue;
+                    }
                 }
-                if(color[i][j] == 1) {
+                if(color[i][j] == white) {
                     d[i + j * 8].draw();
+                }
+                if(color[i][j] == blue) {
+                    d[i + j * 8].draw(Palette::Skyblue);
                 }
             }
         }
     }
+    // ゲームを起動した時に石を4つある状態で始めれるようにする。
 }
